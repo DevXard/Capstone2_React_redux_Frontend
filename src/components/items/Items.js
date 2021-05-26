c1021649450bb34d850edc2161507c7f168fb3f4
Pavlin <forex.xard@gmail.com>
2021-05-26 11:03:27 -0400

    Added items component

--- src/components/items/Items.js
+++ src/components/items/Items.js
@@ -1,6 +1,8 @@
 import {useEffect} from 'react'
 import {useDispatch, useSelector} from 'react-redux';
 import {getAllItems, itemsSelector} from "../../store/slices/itemsSlice"
+import ItemsCard from './ItemsCard';
+import './items.css'
 
 const Items = () => {
 
@@ -13,10 +15,12 @@ const Items = () => {
 
      
     return(
-        <div>
-        <h1>hi</h1>
-           {items.map(item => <div key={item.id}>{item.type}</div>)}
+        <div className="flex justify-center ">
+            <div className=" w-11/12 grid grid-rows-1 grid-flow-col gap-4 overflow-x-scroll shadow-xl rounded-xl scroll-hide border border-gray-200">
+            {items.map(item => <ItemsCard key={item.id} item={item}></ItemsCard>)}
+            </div>
         </div>
+        
     )
 }
 
---
+++ src/components/items/ItemsCard.js
@@ -0,0 +1,27 @@
+const ItemsCard = ({item}) => {
+    console.log(item)
+
+    let isoDate = new Date(item.date)
+
+    let date = `${isoDate.getFullYear()}/${isoDate.getMonth()}/${isoDate.getDate()}`
+
+    console.log(date)
+    return (
+        <div class="bg-gray-100 rounded-xl p-8 m-4 md:p-0 w-60 h-96 shadow-lg border border-gray-200">
+            <img class="w-full p-2 h-44 rounded-xl" 
+            src="https://c8.alamy.com/comp/T58125/selection-of-fruit-and-vegetables-on-white-background-T58125.jpg" />
+            <h1 class="text-center text-2xl font-bold">{item.name}</h1>
+            <h1 class="text-center text-lg">{item.type}</h1>
+            
+            <h1 class="text-left pl-2 pt-2 text-base">Price: ${item.price}</h1>
+            <h1 class="text-left pl-2 text-base">Quantity: ${item.quantity}</h1>
+            <div>
+                <p class="text-center text-base">{item.details}</p>
+            </div>
+            <p class="text-right pr-2 text-sm">Date: {date}</p>
+            
+        </div>
+    )
+}
+
+export default ItemsCard;
